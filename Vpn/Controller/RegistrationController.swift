7a58336464649aff09f189a9dbd4682b5331a95b
Denim <dagn456@mail.ru>
2023-03-31 16:06:57 +0400

    Добавлены TextField поля для регистрации

--- Vpn/Controller/RegistrationController.swift
+++ Vpn/Controller/RegistrationController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class RegistrationController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailTextField.layer.cornerRadius = 30.00
+        passwordTextField.layer.cornerRadius = 30.00
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
