c8b13a5b64d8460feda07e58855592985c50a6fb
Denim <dagn456@mail.ru>
2023-04-05 18:35:10 +0400

    Улучшено меню настроек

---
+++ Vpn/Controller/MenuViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MenuViewController.swift
+//  Vpn
+//
+//  Created by Деним Мержан on 05.04.23.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController, UITableViewDataSource {
+
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var menuCell = ["Поддержка","Ответы на вопросы", "Пользовательское соглашение","Политика конфиденциальности"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self /// Устанавливаем себя в качестве делегата
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Yeha")
+        return menuCell.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        cell.textLabel?.text = menuCell[indexPath.row]
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+}
